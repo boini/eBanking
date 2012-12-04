976f0bbf944fb2110ea5c8237d8334b8dabee84d
Ilya Chaplin <iamchrleych@gmail.com>
2012-12-05 01:28:22 +0300

    added card utils class

---
+++ eBanking-ws/src/main/java/com/ebanking/ws/utils/CardUtils.java
@@ -0,0 +1,11 @@
+package com.ebanking.ws.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: charley
+ * Date: 04.12.12
+ * Time: 22:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class CardUtils {
+}
