4a891ffe979a28936147e53a69660dbdcb9ac51c
Ilya Chaplin <iamchrleych@gmail.com>
2012-12-13 01:35:11 +0300

    added currency conversion

---
+++ eBanking-ws/src/main/java/com/ebanking/ws/utils/TransferUtils.java
@@ -0,0 +1,39 @@
+package com.ebanking.ws.utils;
+
+import com.ebanking.ws.dao.RateDAO;
+import com.ebanking.ws.model.finance.Currency;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: charley
+ * Date: 13.12.12
+ * Time: 0:30
+ * To change this template use File | Settings | File Templates.
+ */
+public class TransferUtils {
+
+    public static final String CURRENCY_CODE_BYR = "BYR";
+
+    @Autowired
+    private RateDAO rateDAO;
+
+    public double convert(Currency inCurrency, Currency outCurrency, double amount) {
+
+        boolean inCurrencyIsByr = CURRENCY_CODE_BYR.equals(inCurrency.getCurrencyCode());
+        boolean outCurrencyIsByr = CURRENCY_CODE_BYR.equals(outCurrency.getCurrencyCode());
+        double rateCurToBr = inCurrencyIsByr ? 1.0 : rateDAO.getByCurrency(inCurrency).getPurchase();
+        double rateBrToCur = outCurrencyIsByr ? 1.0 : rateDAO.getByCurrency(outCurrency).getSale();
+
+        double result = amount * rateCurToBr / rateBrToCur;
+
+        //round to two decimal places
+        BigDecimal bd = BigDecimal.valueOf(result);
+        bd = bd.setScale(2, BigDecimal.ROUND_HALF_UP);
+
+        return bd.doubleValue();
+    }
+
+}
