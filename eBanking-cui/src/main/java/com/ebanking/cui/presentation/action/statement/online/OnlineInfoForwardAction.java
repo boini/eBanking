0d8125255bd4c18f2e50af0c0c780fc7b4969611
Ilya Chaplin <iamchrleych@gmail.com>
2012-12-13 18:25:41 +0300

    struts settings updated

--- eBanking-cui/src/main/java/com/ebanking/cui/presentation/action/statement/online/OnlineInfoForwardAction.java
+++ eBanking-cui/src/main/java/com/ebanking/cui/presentation/action/statement/online/OnlineInfoForwardAction.java
@@ -1,11 +1,6 @@
 package com.ebanking.cui.presentation.action.statement.online;
 
-import com.ebanking.cui.session.HttpSessionUtil;
-import com.ebanking.cui.session.UserHttpSession;
-import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
-
-import java.util.Map;
 
 public class OnlineInfoForwardAction extends ActionSupport {
     public String execute() throws Exception {
